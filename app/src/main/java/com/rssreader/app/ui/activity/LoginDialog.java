2307b7713755b5413e45de1eae72be096bf164e2
罗昶安 <hzluochangan@corp.netease.com>
2016-04-22 13:06:54 +0800

    添加第三方微博登录和qq登录

--- app/src/main/java/com/rssreader/app/ui/activity/LoginDialog.java
+++ app/src/main/java/com/rssreader/app/ui/activity/LoginDialog.java
@@ -5,18 +5,28 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.text.TextUtils;
 import android.widget.Toast;
 
 import com.rssreader.app.commons.AppContext;
-import com.rssreader.app.commons.UMHelper;
 import com.rssreader.app.commons.util.ToastUtil;
 import com.rssreader.app.ui.R;
 import com.umeng.socialize.bean.SHARE_MEDIA;
 import com.umeng.socialize.bean.SocializeEntity;
+import com.umeng.socialize.bean.StatusCode;
+import com.umeng.socialize.controller.UMServiceFactory;
 import com.umeng.socialize.controller.UMSocialService;
+import com.umeng.socialize.controller.listener.SocializeListeners;
 import com.umeng.socialize.controller.listener.SocializeListeners.SocializeClientListener;
+import com.umeng.socialize.exception.SocializeException;
+import com.umeng.socialize.sso.QZoneSsoHandler;
+import com.umeng.socialize.sso.UMQQSsoHandler;
+import com.umeng.socialize.sso.UMSsoHandler;
+
+import java.util.Map;
 
 /**
  * @author LuoChangAn
@@ -25,18 +35,22 @@ public class LoginDialog extends DialogFragment
 {
 
 	public static final String tag = "LoginDialog";
+    public static final String DESCRIPTOR = "com.rsssreader.ui.app";
 	private Activity mActivity;
 	private static final int POS_SINA_WEIBO = 0;
-	private static final int POS_QQZONE = 1;
-	private static final int POS_RENREN = 2;
-	private UMSocialService mController = UMHelper.getUMSocialService(getActivity());
-
+    private static final int POS_SINA_WEIBO_LOGOUT = 1;
+	private static final int POS_QQZONE = 2;
+	private static final int POS_QQZONE_LOGOUT = 3;
+    // 整个平台的Controller, 负责管理整个SDK的配置、操作等处理
+    private UMSocialService mController = UMServiceFactory
+            .getUMSocialService(DESCRIPTOR);
 	
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState)
 	{
 		mActivity = getActivity();
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        addQZoneQQPlatform();
 		builder.setItems(R.array.login_accounts, new OnClickListener()
 		{
 			@Override
@@ -48,25 +62,116 @@ public class LoginDialog extends DialogFragment
 					return;
 				}
 				
-				ScializeMonitor monitor = new ScializeMonitor();
-				switch(which)
-				{
-				case POS_SINA_WEIBO:
-					mController.login(mActivity, SHARE_MEDIA.SINA, monitor);
-					break;
-				case POS_QQZONE:
-					mController.login(mActivity, SHARE_MEDIA.QQ, monitor);
-					break;
-				case POS_RENREN:
-					mController.login(mActivity, SHARE_MEDIA.RENREN, monitor);
-					break;
+				switch(which) {
+                    case POS_SINA_WEIBO:
+                        login(SHARE_MEDIA.SINA);
+                        break;
+                    case POS_SINA_WEIBO_LOGOUT:
+                        logout(SHARE_MEDIA.SINA);
+                        break;
+                    case POS_QQZONE:
+                        login(SHARE_MEDIA.QQ);
+                        break;
+                    case POS_QQZONE_LOGOUT:
+                        logout(SHARE_MEDIA.QQ);
+                        break;
 				}
 			}
 		});
 		return builder.create();
 	}
-	
-	private final class ScializeMonitor implements SocializeClientListener
+
+    private void login(final SHARE_MEDIA platform) {
+        mController.doOauthVerify(getActivity(), platform, new SocializeListeners.UMAuthListener() {
+
+            @Override
+            public void onStart(SHARE_MEDIA platform) {
+                ToastUtil.makeShortToast("start");
+            }
+
+            @Override
+            public void onError(SocializeException e, SHARE_MEDIA platform) {
+            }
+
+            @Override
+            public void onComplete(Bundle value, SHARE_MEDIA platform) {
+                ToastUtil.makeShortToast("onComplete");
+                String uid = value.getString("uid");
+                if (!TextUtils.isEmpty(uid)) {
+                    getUserInfo(platform);
+                } else {
+                    ToastUtil.makeShortToast("授权失败...");
+                }
+            }
+
+            @Override
+            public void onCancel(SHARE_MEDIA platform) {
+            }
+        });
+    }
+
+
+    /**
+     * 注销本次登录</br>
+     */
+    private void logout(final SHARE_MEDIA platform) {
+        mController.deleteOauth(mActivity, platform, new SocializeClientListener() {
+
+            @Override
+            public void onStart() {
+
+            }
+
+            @Override
+            public void onComplete(int status, SocializeEntity entity) {
+                String showText = "解除" + platform.toString() + "平台授权成功";
+                if (status != StatusCode.ST_CODE_SUCCESSED) {
+                    showText = "解除" + platform.toString() + "平台授权失败[" + status + "]";
+                }
+                Toast.makeText(getActivity(), showText, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    /**
+     * 获取授权平台的用户信息</br>
+     */
+    private void getUserInfo(SHARE_MEDIA platform) {
+        mController.getPlatformInfo(mActivity, platform, new SocializeListeners.UMDataListener() {
+
+            @Override
+            public void onStart() {
+
+            }
+
+            @Override
+            public void onComplete(int status, Map<String, Object> info) {
+                if (info != null) {
+                    ToastUtil.makeLongToast(info.toString());
+                }
+            }
+        });
+    }
+
+
+
+    private void addQZoneQQPlatform() {
+        String appId = "1105267281";
+        String appKey = "pWvrIZZ2VSDTBQUk";
+        // 添加QQ支持, 并且设置QQ分享内容的target url
+        UMQQSsoHandler qqSsoHandler = new UMQQSsoHandler(getActivity(),
+                appId, appKey);
+        qqSsoHandler.setTargetUrl("http://www.umeng.com");
+        qqSsoHandler.addToSocialSDK();
+
+        // 添加QZone平台
+        QZoneSsoHandler qZoneSsoHandler = new QZoneSsoHandler(getActivity(), appId, appKey);
+        qZoneSsoHandler.addToSocialSDK();
+    }
+
+
+    private final class ScializeMonitor implements SocializeClientListener
 	{
 
 		@Override
@@ -87,4 +192,14 @@ public class LoginDialog extends DialogFragment
 		}
 		
 	}
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        UMSsoHandler ssoHandler = mController.getConfig().getSsoHandler(
+                requestCode);
+        if (ssoHandler != null) {
+            ssoHandler.authorizeCallBack(requestCode, resultCode, data);
+        }
+    }
 }
